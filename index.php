f8a2976b77abab44d7b58656e56661e54adf5ddf
JakeSmokie <jakesmokie@gmail.com>
2018-09-02 03:40:34 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>ПИП</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="images/icons/skull.ico" sizes="">
+</head>
+<body class="custom_body">
+<table class="body_table">
+    <tr>
+        <td width="225px"></td>
+        <td width="225px" height="150px" rowspan="2" class="c_1">
+            <p>Айгузин Иван Олегович</p>
+            <p>P3218</p>
+            <p>Вариант 28801</p>
+        </td>
+        <td height="50px" width="225px"></td>
+        <td width="450px" class="c_2">Университет ИТМО</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+    <tr>
+        <td height="150px"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td height="225px" class="c_5"></td>
+        <td></td>
+        <td height="450px" rowspan="3" class="c_3"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td height="150px" class="c_4"></td>
+        <td></td>
+    </tr>
+<!--    <tr>-->
+<!--        <td height="600px"></td>-->
+<!--    </tr>-->
+</table>
+<footer>
+
+</footer>
+</body>
+</html>
